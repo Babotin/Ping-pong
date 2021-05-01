1976418a774392907f54691b186c8671f0e886f2
Babotin <83505360+Babotin@users.noreply.github.com>
2021-05-01 18:42:16 +0300

    Add files via upload
    class Ball

--- ping-pong.py
+++ ping-pong.py
@@ -5,6 +5,31 @@ h = 700
 
 window = display.set_mode((w, h))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, file, W, H, x, y, speed_x, speed_y):
+        #sprite.Sprite.__init__(self)
+        super().__init__()
+        self.file = image.load(file)
+        self.file = transform.scale(self.file, (W, H))
+        self.rect = self.file.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+    def draw(self):
+        window.blit(self.file, self.rect)
+
+class Ball(GameSprite):
+    def move(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.x > w - 40 or self.rect.x < 0:
+            self.speed_x *= -1
+        if self.rect.y > h - 40 or self.rect.y < 0:
+            self.speed_y *= -1
+
+ball = Ball("ball.png", 40, 40, w / 2, h / 2, 6, 6)
+
 clock = time.Clock()
 
 game = True
@@ -13,5 +38,7 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    ball.move()
+    ball.draw()
     display.update()
     clock.tick(50)
